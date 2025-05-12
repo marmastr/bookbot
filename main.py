8888e0c6b1b08e2f793dc203fda6ab4d93dfb0e0
marmastr <marmastr@gmail.com>
2025-05-12 16:00:00 +0000

    book path is now pass as cli argument

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from stats import get_num_words, get_char_dict_sorted
+import sys
 
-book_name = "frankenstein"
-book_path = f"books/{book_name}.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+# book_name = "frankenstein"
+# book_path = f"books/{book_name}.txt"
+book_path = sys.argv[1]
 
 
 def get_book_text(path):
